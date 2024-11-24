fd86da6734b56d873f2093d0a1c3f6ea85f78101
Ross Hill <ross@rosshill.ca>
2024-11-23 22:38:41 -0500

    fix: resolve lint issue in src/parse-rc-file.ts
    Corrected linting errors in src/parse-rc-file.ts to ensure compliance with code style guidelines. Also resolved other auto-fixable lint errors.

--- test/parse-args.spec.ts
+++ test/parse-args.spec.ts
@@ -27,13 +27,13 @@ describe('parseArgs', (): void => {
   it('should parse environment value', (): void => {
     const res = parseArgs(['-e', environments[0], command])
     assert.exists(res.rc)
-    assert.sameOrderedMembers(res.rc!.environments, [environments[0]])
+    assert.sameOrderedMembers(res.rc.environments, [environments[0]])
   })
 
   it('should parse multiple environment values', (): void => {
     const res = parseArgs(['-e', environments.join(','), command])
     assert.exists(res.rc)
-    assert.sameOrderedMembers(res.rc!.environments, environments)
+    assert.sameOrderedMembers(res.rc.environments, environments)
   })
 
   it('should parse command value', (): void => {
@@ -59,31 +59,31 @@ describe('parseArgs', (): void => {
   it('should parse override option', (): void => {
     const res = parseArgs(['-e', environments[0], '--no-override', command, ...commandArgs])
     assert.exists(res.options)
-    assert.isTrue(res.options!.noOverride)
+    assert.isTrue(res.options.noOverride)
   })
 
   it('should parse use shell option', (): void => {
     const res = parseArgs(['-e', environments[0], '--use-shell', command, ...commandArgs])
     assert.exists(res.options)
-    assert.isTrue(res.options!.useShell)
+    assert.isTrue(res.options.useShell)
   })
 
   it('should parse rc file path', (): void => {
     const res = parseArgs(['-e', environments[0], '-r', rcFilePath, command, ...commandArgs])
     assert.exists(res.rc)
-    assert.equal(res.rc!.filePath, rcFilePath)
+    assert.equal(res.rc.filePath, rcFilePath)
   })
 
   it('should parse env file path', (): void => {
     const res = parseArgs(['-f', envFilePath, command, ...commandArgs])
     assert.exists(res.envFile)
-    assert.equal(res.envFile!.filePath, envFilePath)
+    assert.equal(res.envFile.filePath, envFilePath)
   })
 
   it('should parse fallback option', (): void => {
     const res = parseArgs(['-f', envFilePath, '--fallback', command, ...commandArgs])
     assert.exists(res.envFile)
-    assert.isTrue(res.envFile!.fallback)
+    assert.isTrue(res.envFile.fallback)
   })
 
   it('should print to console.info if --verbose flag is passed', (): void => {
