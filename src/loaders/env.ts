701a6e56bedf99ed3d0586ceb4ec14557cde4441
Kyle Hensel <k.y.l.e@outlook.co.nz>
2024-12-11 18:31:39 +1100

    refactor: made adding a new loader easier
    Co-authored-by: KilianKilmister <KilianKilmister@users.noreply.github.com>

---
+++ src/loaders/env.ts
@@ -0,0 +1,65 @@
+import type { Environment } from '../types.js'
+
+/**
+ * Parse out all env vars from a given env file string and return an object
+ */
+export function parseEnvString(envFileString: string): Environment {
+  // First thing we do is stripe out all comments
+  envFileString = stripComments(envFileString.toString())
+
+  // Next we stripe out all the empty lines
+  envFileString = stripEmptyLines(envFileString)
+
+  // Merge the file env vars with the current process env vars (the file vars overwrite process vars)
+  return parseEnvVars(envFileString)
+}
+
+/**
+ * Parse out all env vars from an env file string
+ */
+export function parseEnvVars(envString: string): Environment {
+  const envParseRegex = /^((.+?)[=](.*))$/gim
+  const matches: Environment = {}
+  let match
+  while ((match = envParseRegex.exec(envString)) !== null) {
+    // Note: match[1] is the full env=var line
+    const key = match[2].trim()
+    let value: string | number | boolean = match[3].trim()
+
+    // remove any surrounding quotes
+    value = value
+      .replace(/(^['"]|['"]$)/g, '')
+      .replace(/\\n/g, '\n')
+
+    // Convert string to JS type if appropriate
+    if (value !== '' && !isNaN(+value)) {
+      matches[key] = +value
+    }
+    else if (value === 'true') {
+      matches[key] = true
+    }
+    else if (value === 'false') {
+      matches[key] = false
+    }
+    else {
+      matches[key] = value
+    }
+  }
+  return JSON.parse(JSON.stringify(matches)) as Environment
+}
+
+/**
+ * Strips out comments from env file string
+ */
+export function stripComments(envString: string): string {
+  const commentsRegex = /(^\s*#.*$)/gim
+  return envString.replace(commentsRegex, '')
+}
+
+/**
+ * Strips out newlines from env file string
+ */
+export function stripEmptyLines(envString: string): string {
+  const emptyLinesRegex = /(^\n)/gim
+  return envString.replace(emptyLinesRegex, '')
+}
--- src/parse-env-file.ts
+++ src/parse-env-file.ts
@@ -3,6 +3,7 @@ import { extname } from 'node:path'
 import { pathToFileURL } from 'node:url'
 import { resolveEnvFilePath, IMPORT_HOOK_EXTENSIONS, isPromise } from './utils.js'
 import type { Environment } from './types.ts'
+import { parseEnvString } from './loaders/env.js';
 
 /**
  * Gets the environment vars from an env file
@@ -34,74 +35,13 @@ export async function getEnvFileVars(envFilePath: string): Promise<Environment> 
     if (isPromise(env)) {
       env = await env
     }
+    return env;
   }
-  else {
-    const file = readFileSync(absolutePath, { encoding: 'utf8' })
-    env = parseEnvString(file)
+
+  const file = readFileSync(absolutePath, { encoding: 'utf8' })
+
+  switch (ext) {
+    default:
+      return parseEnvString(file)
   }
-  return env
-}
-
-/**
- * Parse out all env vars from a given env file string and return an object
- */
-export function parseEnvString(envFileString: string): Environment {
-  // First thing we do is stripe out all comments
-  envFileString = stripComments(envFileString.toString())
-
-  // Next we stripe out all the empty lines
-  envFileString = stripEmptyLines(envFileString)
-
-  // Merge the file env vars with the current process env vars (the file vars overwrite process vars)
-  return parseEnvVars(envFileString)
-}
-
-/**
- * Parse out all env vars from an env file string
- */
-export function parseEnvVars(envString: string): Environment {
-  const envParseRegex = /^((.+?)[=](.*))$/gim
-  const matches: Environment = {}
-  let match
-  while ((match = envParseRegex.exec(envString)) !== null) {
-    // Note: match[1] is the full env=var line
-    const key = match[2].trim()
-    let value: string | number | boolean = match[3].trim()
-
-    // remove any surrounding quotes
-    value = value
-      .replace(/(^['"]|['"]$)/g, '')
-      .replace(/\\n/g, '\n')
-
-    // Convert string to JS type if appropriate
-    if (value !== '' && !isNaN(+value)) {
-      matches[key] = +value
-    }
-    else if (value === 'true') {
-      matches[key] = true
-    }
-    else if (value === 'false') {
-      matches[key] = false
-    }
-    else {
-      matches[key] = value
-    }
-  }
-  return JSON.parse(JSON.stringify(matches)) as Environment
-}
-
-/**
- * Strips out comments from env file string
- */
-export function stripComments(envString: string): string {
-  const commentsRegex = /(^\s*#.*$)/gim
-  return envString.replace(commentsRegex, '')
-}
-
-/**
- * Strips out newlines from env file string
- */
-export function stripEmptyLines(envString: string): string {
-  const emptyLinesRegex = /(^\n)/gim
-  return envString.replace(emptyLinesRegex, '')
 }
